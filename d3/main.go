773d19e71f6b435d4be4883b7af65b16bed55fe6
ikiris <4852950+ikiris@users.noreply.github.com>
2024-12-03 00:29:13 -0600

    d3 gross

--- d3/main.go
+++ d3/main.go
@@ -33,3 +33,48 @@ func p1(r io.Reader) (int64, error) {
 
 	return ttl, nil
 }
+
+func p2(r io.Reader) (int64, error) {
+	s := bufio.NewScanner(r)
+
+	r1 := regexp.MustCompile(`(mul\(([0-9]+),([0-9]+)\)|do\(\)|don't\(\))`) // yes i should have used a lexer parser, but i wanted to see if i could be lazy
+
+	var ttl int64
+
+	enabled := true
+
+	for s.Scan() {
+		r := r1.FindAllStringSubmatch(s.Text(), -1) // Lazy way. lets hope.
+		for _, v := range r {
+			if v[0] == "do()" {
+				enabled = true
+
+				continue
+			}
+
+			if v[0] == "don't()" {
+				enabled = false
+
+				continue
+			}
+
+			if !enabled {
+				continue
+			}
+
+			m1, err := strconv.ParseInt(v[2], 10, 64)
+			if err != nil {
+				return 0, err
+			}
+
+			m2, err := strconv.ParseInt(v[3], 10, 64)
+			if err != nil {
+				return 0, err
+			}
+
+			ttl += m1 * m2
+		}
+	}
+
+	return ttl, nil
+}
--- d3/main_test.go
+++ d3/main_test.go
@@ -41,3 +41,38 @@ func TestD3P1(t *testing.T) {
 		})
 	}
 }
+
+func TestD3P2(t *testing.T) {
+	tests := []struct {
+		name    string
+		data    io.Reader
+		want    int64
+		wantErr bool
+	}{
+		{
+			"basic",
+			testgeneric.GetHandle(t, "testdata/basic.txt"),
+			161,
+			false,
+		},
+		{
+			"basic",
+			testgeneric.GetHandle(t, "testdata/d3p1.txt"),
+			0,
+			false,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := p2(tc.data)
+			if (err != nil) != tc.wantErr {
+				t.Fatalf("func %s goterr: %v wanted: %v", tc.name, err, tc.wantErr)
+			}
+
+			if got != tc.want {
+				t.Fatalf("func got: %d, want: %d", got, tc.want)
+			}
+		})
+	}
+}
