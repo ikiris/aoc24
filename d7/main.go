014284f26e62145b02a81c76d2abb2ccb5cbe7b1
ikiris <4852950+ikiris@users.noreply.github.com>
2024-12-07 17:57:05 -0600

    d7 p2

--- d7/main.go
+++ d7/main.go
@@ -65,3 +65,79 @@ func tryget(desired int64, nums []int64) bool {
 
 	return tryget(desired, numsc)
 }
+
+func intConcat(a, b int64) (int64, error) {
+	str := fmt.Sprintf("%d%d", a, b)
+
+	return strconv.ParseInt(str, 10, 64)
+}
+
+func trygetB(desired int64, nums []int64) bool {
+	if len(nums) == 1 {
+		return nums[0] == desired
+	}
+
+	numsb := []int64{nums[0] + nums[1]}
+	numsb = append(numsb, nums[2:]...)
+
+	if trygetB(desired, numsb) {
+		return true
+	}
+
+	numsc := []int64{nums[0] * nums[1]}
+	numsc = append(numsc, nums[2:]...)
+
+	if trygetB(desired, numsc) {
+		return true
+	}
+
+	i, err := intConcat(nums[0], nums[1])
+	if err != nil {
+		panic(err)
+	}
+
+	numsd := []int64{i}
+	numsd = append(numsd, nums[2:]...)
+
+	return trygetB(desired, numsd)
+}
+
+func p2(r io.Reader) (int64, error) {
+	s := bufio.NewScanner(r)
+
+	var ttl int64
+
+	for s.Scan() {
+		t1 := strings.Split(s.Text(), ":")
+
+		desired, err := strconv.ParseInt(t1[0], 10, 64)
+		if err != nil {
+			return 0, err
+		}
+
+		textNums := strings.Split(t1[1], " ")
+
+		nums := []int64{}
+
+		for _, v := range textNums {
+			if v == "" {
+				continue
+			}
+
+			n, err := strconv.ParseInt(v, 10, 64)
+			if err != nil {
+				return 0, fmt.Errorf("failed to parse: %w", err)
+			}
+
+			nums = append(nums, n)
+		}
+
+		if !trygetB(desired, nums) {
+			continue
+		}
+
+		ttl += desired
+	}
+
+	return ttl, nil
+}
--- d7/main_test.go
+++ d7/main_test.go
@@ -40,3 +40,37 @@ func TestD7P1(t *testing.T) {
 		})
 	}
 }
+
+func TestD7P2(t *testing.T) {
+	tests := []struct {
+		name    string
+		data    io.Reader
+		want    int64
+		wantErr bool
+	}{
+		{
+			"basic",
+			testgeneric.GetHandle(t, "testdata/basic.txt"),
+			11387,
+			false,
+		},
+		{
+			"d7p2",
+			testgeneric.GetHandle(t, "testdata/d7p1.txt"),
+			286580387663654,
+			false,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := p2(tc.data)
+			if (err != nil) != tc.wantErr {
+				t.Fatalf("func %s goterr: %v wanted: %v", tc.name, err, tc.wantErr)
+			}
+
+			if got != tc.want {
+				t.Fatalf("func got: %d, want: %d", got, tc.want)
+			}
+		})
+	}
+}
